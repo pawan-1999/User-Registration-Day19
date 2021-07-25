d20d5e87044129ef63557b9ab16cfc86a5eefd3b
pawan-1999 <guptapawan767@gmail.com>
2021-07-25 11:57:41 +0530

    Added : Validation Of User Last Name

--- User-Registration-Day19/UserRegistrationValidation.cs
+++ User-Registration-Day19/UserRegistrationValidation.cs
@@ -16,5 +16,15 @@ namespace User_Registration_Day19
             return Regex.IsMatch(Firstname, Regex_firstName);
         }
         #endregion UC-1
+
+        #region UC-2
+        public static string Regex_lastName = "^[A-Z][A-Za-z]{2,}$";
+
+        public static bool validatelastName(string Lastname)
+        {
+            Console.WriteLine("Last Name Validation is T/F :");
+            return Regex.IsMatch(Lastname, Regex_lastName);
+        }
+        #endregion UC-2
     }
 }
