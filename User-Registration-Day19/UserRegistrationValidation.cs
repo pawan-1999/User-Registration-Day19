92af053ede821ac3ccda3191b5b216443d164b8a
pawan-1999 <guptapawan767@gmail.com>
2021-07-25 11:52:11 +0530

    Added : Validation Of User First Name

---
+++ User-Registration-Day19/UserRegistrationValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace User_Registration_Day19
+{
+    class UserRegistrationValidation
+    {
+        #region UC-1
+        public static string Regex_firstName = "^[A-Z][A-Za-z]{2,}$";
+
+        public static bool validatefisrtName(string Firstname)
+        {
+            Console.WriteLine("First Name Validation is T/F :");
+            return Regex.IsMatch(Firstname, Regex_firstName);
+        }
+        #endregion UC-1
+    }
+}
