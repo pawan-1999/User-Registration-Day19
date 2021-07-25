a01ab461e32eb249e70fbbdc7c917e64c0d58efb
pawan-1999 <guptapawan767@gmail.com>
2021-07-25 11:56:57 +0530

    Added : Main Method To Take User Input

--- User-Registration-Day19/Program.cs
+++ User-Registration-Day19/Program.cs
@@ -7,10 +7,13 @@ namespace User_Registration_Day19
         static void Main(string[] args)
         {
             Console.WriteLine("Checking Validation of User Input : ");
-            Console.WriteLine("Enter the First Name:");
+            Console.WriteLine("Enter the First Name :");
             string firstName = Console.ReadLine();
             Console.WriteLine(UserRegistrationValidation.validatefisrtName(firstName));
-           
+            Console.WriteLine("Enter the Last Name : ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine(UserRegistrationValidation.validatelastName(lastName));
+
         }
     }
 }
