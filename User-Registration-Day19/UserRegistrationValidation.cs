5ccd62b1f7477e4b8a9326b4dc0d753ff54ac5df
pawan-1999 <guptapawan767@gmail.com>
2021-07-28 18:44:16 +0530

    Added : Regex Validation For User Input And Testing

--- User-Registration-Day19/UserRegistrationValidation.cs
+++ User-Registration-Day19/UserRegistrationValidation.cs
@@ -12,7 +12,6 @@ namespace User_Registration_Day19
 
         public  bool validatefisrtName(string Firstname)
         {
-            Console.WriteLine("First Name Validation is T/F :");
             return Regex.IsMatch(Firstname, Regex_firstName);
         }
         #endregion UC-1
@@ -22,7 +21,6 @@ namespace User_Registration_Day19
 
         public  bool validatelastName(string Lastname)
         {
-            Console.WriteLine("Last Name Validation is T/F :");
             return Regex.IsMatch(Lastname, Regex_lastName);
         }
         #endregion UC-2
@@ -32,7 +30,6 @@ namespace User_Registration_Day19
 
         public  bool validateEmail(string email)
         {
-            Console.WriteLine("Email Id Validation is T/F :");
             return Regex.IsMatch(email, Regex_Email);
         }
         #endregion UC-3
@@ -40,9 +37,8 @@ namespace User_Registration_Day19
         #region UC-4
         public static string Regex_ph_No = "^([1-9]{1,2}[ ])?[6-9][0-9]{9}$";
 
-        public  bool validateph_No(string Ph_no)
+        public  bool validatePh_No(string Ph_no)
         {
-            Console.WriteLine("Phone No Validation is T/F :");
             return Regex.IsMatch(Ph_no, Regex_ph_No);
         }
         #endregion UC-4
@@ -50,9 +46,8 @@ namespace User_Registration_Day19
         #region UC-8
         public static string Regex_password = "((?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%]).[A-Z0-9a-z@#$%]{8,})";
 
-        public  bool validatepassword(string Password)
+        public  bool validatePassword(string Password)
         {
-            Console.WriteLine("Password Validation is T/F :");
             return Regex.IsMatch(Password, Regex_password);
         }
         #endregion UC-8
