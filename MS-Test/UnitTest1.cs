6d71a7d6f36ef226746208ec0ad75ca9a36534c8
pawan-1999 <guptapawan767@gmail.com>
2021-07-28 18:43:51 +0530

    Added : Testing For User Registration Validation

--- MS-Test/UnitTest1.cs
+++ MS-Test/UnitTest1.cs
@@ -12,6 +12,7 @@ namespace MS_Test
         {
             userregistrationvalidation = new UserRegistrationValidation();
         }
+
         #region UC-1
         [TestMethod]
         public void TestUserFirstName()
@@ -21,5 +22,42 @@ namespace MS_Test
         }
         #endregion UC-1
 
+        #region UC-2
+        [TestMethod]
+        public void TestUserLastName()
+        {
+            var result = userregistrationvalidation.validatelastName("Gupta");
+            Assert.AreEqual(true, result);
+        }
+        #endregion UC-2
+        
+        #region UC-3
+        [TestMethod]
+        public void TestUserEmailName()
+        {
+            var result = userregistrationvalidation.validateEmail("guptapawan767@gmail.com");
+            Assert.AreEqual(true, result);
+        }
+        #endregion UC-3
+
+        #region UC-4
+        [TestMethod]
+        public void TestUserPh_no()
+        {
+            var result = userregistrationvalidation.validatePh_No("91 8484053791");
+            Assert.AreEqual(true, result);
+        }
+        #endregion UC-4
+        
+        #region UC 5-8
+        [TestMethod]
+        public void TestUserPassword()
+        {
+            var result = userregistrationvalidation.validatePassword("Pawan@1999");
+            Assert.AreEqual(true, result);
+        }
+        #endregion UC 5-8
+
+       
     }
 }
