061b1675a1a38e9e51c66be6761ee24832889262
pawan-1999 <guptapawan767@gmail.com>
2021-08-01 00:24:22 +0530

    Added : To Define Custom Exception

---
+++ User-Registration-Day19/UserRegistrationCustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User_Registration_Day19
+{
+    public class UserRegistrationCustomException : Exception
+    {
+        private readonly ExceptionType type;
+
+        public enum ExceptionType
+        {
+            NULL_MESSAGE,
+            EMPTY_MESSAGE,
+            INVALID_INPUT
+        }
+
+
+        public UserRegistrationCustomException(ExceptionType Type, string message) : base(message)
+        {
+            this.type = Type;
+        }
+
+    }
+}
