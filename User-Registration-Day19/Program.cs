f3e1e0559caee08817d2e1c3023c30cf0c966709
pawan-1999 <guptapawan767@gmail.com>
2021-08-01 00:23:18 +0530

    Added : Main Method To Pass User Input

--- User-Registration-Day19/Program.cs
+++ User-Registration-Day19/Program.cs
@@ -6,26 +6,76 @@ namespace User_Registration_Day19
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Checking Validation of User Input : ");
-            //Console.WriteLine("Enter the First Name :");
-            //string firstName = Console.ReadLine();
-            //Console.WriteLine(UserRegistrationValidation.validatefisrtName(firstName));
-            //Console.WriteLine("Enter the Last Name : ");
-            //string lastName = Console.ReadLine();
-            //Console.WriteLine(UserRegistrationValidation.validatelastName(lastName));
-            //Console.WriteLine("Enter the Email Id : ");
-            //string emailId = Console.ReadLine();
-            //Console.WriteLine(UserRegistrationValidation.validateEmail(emailId));
-            //Console.WriteLine("Enter the Phone No : ");
-            //string phoneNo = Console.ReadLine();
-            //Console.WriteLine(UserRegistrationValidation.validateph_No(phoneNo));
-            //Console.WriteLine("Enter the Password : ");
-            //string password = Console.ReadLine();
-            //Console.WriteLine(UserRegistrationValidation.validatepassword(password));
+            UserRegistrationValidation obj = new UserRegistrationValidation();
 
-            //#region UC-9
-            //Console.WriteLine(SampleEmail.validateEmail("abc+100@gmail.com"));
-            //#endregion UC-9
+
+            Console.WriteLine("Checking Validation of User Input : ");
+
+
+            #region UC-1
+            bool Result = false;
+            while (Result != true) 
+            {
+                Console.WriteLine("Enter the First Name :");
+                string firstName = Console.ReadLine();
+                Result = obj.validatefisrtName(firstName);
+            }
+            #endregion UC-1
+
+
+            #region UC-2
+            Result = false;
+            while (Result != true)
+            {
+                Console.WriteLine("Enter the Last Name : ");
+                string lastName = Console.ReadLine();
+                Result=obj.validatelastName(lastName);
+            }
+            #endregion UC-2
+
+
+            #region UC-3
+            Result = false;
+            while (Result != true)
+            {
+                Console.WriteLine("Enter the Email Id : ");
+                string emailId = Console.ReadLine();
+                Result = obj.validateEmail(emailId);
+            }
+            #endregion UC-3
+
+
+            #region UC-4
+            Result = false;
+            while (Result != true)
+            {
+                Console.WriteLine("Enter the Phone No : ");
+                string phoneNo = Console.ReadLine();
+                Result = obj.validatePh_No(phoneNo);
+            }
+            #endregion UC-4
+
+
+            #region UC 5-8
+            Result = false;
+            while (Result != true)
+            {
+                Console.WriteLine("Enter the Password : ");
+                string password = Console.ReadLine();
+                Result = obj.validatePassword(password);
+            }
+            #endregion UC 5-8
+
+
+            #region UC-9
+            Result = false;
+            while (Result != true)
+            {
+                Console.WriteLine("Enter the Email Id : ");
+                string emailId = Console.ReadLine();
+                Result = SampleEmail.validateEmail(emailId);
+            }
+            #endregion UC-9
         }
     }
 }
