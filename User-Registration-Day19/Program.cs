878bf3df4ca11eabd255b7901ee2f1714cf56cd0
pawan-1999 <guptapawan767@gmail.com>
2021-08-05 00:03:49 +0530

    Added : Main Method

--- User-Registration-Day19/Program.cs
+++ User-Registration-Day19/Program.cs
@@ -6,26 +6,12 @@ namespace User_Registration_Day19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Checking Validation of User Input : ");
-            Console.WriteLine("Enter the First Name :");
-            string firstName = Console.ReadLine();
-            Console.WriteLine(UserRegistrationValidation.validatefisrtName(firstName));
-            Console.WriteLine("Enter the Last Name : ");
-            string lastName = Console.ReadLine();
-            Console.WriteLine(UserRegistrationValidation.validatelastName(lastName));
-            Console.WriteLine("Enter the Email Id : ");
-            string emailId = Console.ReadLine();
-            Console.WriteLine(UserRegistrationValidation.validateEmail(emailId));
-            Console.WriteLine("Enter the Phone No : ");
-            string phoneNo = Console.ReadLine();
-            Console.WriteLine(UserRegistrationValidation.validateph_No(phoneNo));
-            Console.WriteLine("Enter the Password : ");
-            string password = Console.ReadLine();
-            Console.WriteLine(UserRegistrationValidation.validatepassword(password));
+            UserRegistrationValidation obj = new UserRegistrationValidation();
 
-            #region UC-9
-            Console.WriteLine(SampleEmail.validateEmail("abc+100@gmail.com"));
-            #endregion UC-9
+            Console.WriteLine("Checking Validation of User Input : ");
+
+            obj.validateFirstName();
+
         }
     }
 }
